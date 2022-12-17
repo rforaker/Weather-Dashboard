7a3dac1b8c8c251619a5449efe4b72d23f6a33c6
rforaker <rtforaker@gmail.com>
2022-12-17 12:39:57 -0500

    added glbal varabiles, function to format date

---
+++ index.js
@@ -0,0 +1,15 @@
+var cityList =$("#city-list");
+var cities = [];
+var key = "65f89a7498f261c289d28f60e4152811";
+
+function FormatDay(date){
+    var date = new Date();
+    console.log(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    
+    var dayOutput = date.getFullYear() + '/' +
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day;
+    return dayOutput;
+}
