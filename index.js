38d3a98b61bab5c4b99acf0b3b785c131a63fc2d
rforaker <rtforaker@gmail.com>
2022-12-17 12:42:02 -0500

    added function to store and render cities

--- index.js
+++ index.js
@@ -13,3 +13,42 @@ function FormatDay(date){
         (day<10 ? '0' : '') + day;
     return dayOutput;
 }
+
+
+
+init();
+
+function init(){
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        cities = storedCities;
+      }
+    renderCities();
+}
+
+function storeCities(){
+  localStorage.setItem("cities", JSON.stringify(cities));
+  console.log(localStorage);
+}
+
+function renderCities() {
+    cityList.empty();
+    
+    for (var i = 0; i < cities.length; i++) {
+      var city = cities[i];
+      
+      var li = $("<li>").text(city);
+      li.attr("id","listC");
+      li.attr("data-city", city);
+      li.attr("class", "list-group-item");
+      console.log(li);
+      cityList.prepend(li);
+    }
+    if (!city){
+        return
+    } 
+    else{
+        getResponseWeather(city)
+    };
+}   
